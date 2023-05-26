194a5fa030f413283923627f6d9f854bac58e98f
pedrovalido <pedro.validex@gmail.com>
2023-05-26 18:57:52 +0100

    test(protocol): sample hardhat test file

---
+++ test/HimalayaCompound.ts
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import hre from "hardhat";
+
+describe("Lock", function () {
+  it("Should test for something", async function () {
+    expect(true).to.equal(true);
+  });
+});
