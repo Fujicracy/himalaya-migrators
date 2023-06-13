1c9fc6e349ecea04280bf26d2c3f6c810d862ec1
pedrovalido <pedro.validex@gmail.com>
2023-06-13 14:41:29 +0800

    test(protocol): use utils for compound testing

--- test/mainnet/HimalayaCompound.t.sol
+++ test/mainnet/HimalayaCompound.t.sol
@@ -14,109 +14,22 @@ import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {IHimalayaMigrator} from "../../src/interfaces/IHimalayaMigrator.sol";
 import {HimalayaCompound} from "../../src/migrators/HimalayaCompound.sol";
 import {ICompoundV3} from "@fuji-v2/src/interfaces/compoundV3/ICompoundV3.sol";
-import {IConnext, IXReceiver} from "@fuji-v2/src/interfaces/connext/IConnext.sol";
+import {HimalayaCompoundUtils} from "../HimalayaCompoundUtils.t.sol";
+import {IXReceiver} from "@fuji-v2/src/interfaces/connext/IConnext.sol";
 
 /**
  * @dev This contract tests the integration of CompoundV2 and CompoundV3.
  */
-contract HimalayaCompoundUnitTests is Test {
-  uint256 public constant ALICE_PK = 0xA;
-  address public ALICE = vm.addr(ALICE_PK);
-
-  address public constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-  address public constant WSTETH = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
-  address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-  address public constant WBTC = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
-
-  CompoundV2 public compoundV2;
-  CompoundV3 public compoundV3;
-
-  IHimalayaMigrator public himalayaCompound;
-
-  IERC20 public cETHV2 = IERC20(0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5);
-  address public cWETHV3 = 0xA17581A9E3356d9A858b789D68B4d866e593aE94;
-  address public cUSDCV3 = 0xc3d688B66703497DAA19211EEdff47f25384cdc3;
-  address public cUSDCV3_Polygon = 0xF25212E676D1F7F89Cd72fFEe66158f541246445;
-
-  address public constant CONNEXT_MAINNET = 0x8898B472C54c31894e3B9bb83cEA802a5d0e63C6;
-
-  uint256 forkIdMainnet;
-
-  uint256 AMOUNT_SUPPLY_WBTC = 1e8;
-  uint256 AMOUNT_BORROW_USDC = 10e8;
+contract HimalayaCompoundUnitTests is HimalayaCompoundUtils {
 
   function setUp() public {
-    forkIdMainnet = vm.createSelectFork("mainnet");
+    vm.createSelectFork("mainnet");
     compoundV2 = new CompoundV2();
     compoundV3 = new CompoundV3();
 
     himalayaCompound = new HimalayaCompound(CONNEXT_MAINNET);
 
-    vm.label(ALICE, "ALICE");
-    vm.label(WETH, "WETH");
-    vm.label(WSTETH, "WSTETH");
-    vm.label(USDC, "USDC");
-    vm.label(WBTC, "WBTC");
-
-    vm.label(address(compoundV2), "compoundV2");
-    vm.label(address(compoundV3), "compoundV3");
-    vm.label(address(himalayaCompound), "himalayaCompound");
-
-    vm.label(address(cETHV2), "cETHV2");
-    vm.label(cWETHV3, "cWETHV3");
-    vm.label(cUSDCV3, "cUSDCV3");
-  }
-
-  function _utils_depositV2(uint256 amount, address asset) private {
-    address cTokenAddr = address(cETHV2);
-
-    _enterCollatMarketV2(cTokenAddr);
-
-    uint256 balanceCTokenBefore = IERC20(cTokenAddr).balanceOf(address(this));
-    if (_isWETH(asset)) {
-      ICETH cETH = ICETH(cTokenAddr);
-      // unwrap WETH to ETH
-      IWETH9(asset).withdraw(amount);
-
-      // cEth reverts if mint unsuccessful
-      cETH.mint{value: amount}();
-    } else {
-      ICERC20 cToken = ICERC20(cTokenAddr);
-
-      cToken.mint(amount);
-    }
-
-    uint256 balanceCTokenAfter = IERC20(cTokenAddr).balanceOf(address(this));
-    IERC20(cTokenAddr).transfer(msg.sender, balanceCTokenAfter - balanceCTokenBefore);
-  }
-
-  function _enterCollatMarketV2(address asset) private {
-    IComptroller comptroller = IComptroller(0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B);
-
-    address[] memory markets = new address[](1);
-    markets[0] = asset;
-    comptroller.enterMarkets(markets);
-  }
-
-  function _isWETH(address asset) private pure returns (bool) {
-    return asset == 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-  }
-
-  function providerNameV2() private pure returns (string memory) {
-    return "Compound_V2";
-  }
-
-  function _utils_depositV3(
-    // address user,
-    uint256 amount,
-    address asset,
-    address cMarketV3
-  )
-    internal
-    returns (bool success)
-  {
-    ICompoundV3(cMarketV3).supply(asset, amount);
-    success = true;
+    setLabels();
   }
 
   function test_handleOutboundFromV2ToV3() public {
@@ -125,7 +38,7 @@ contract HimalayaCompoundUnitTests is Test {
 
     //Deposit 100 WETH into CompoundV2
     vm.startPrank(ALICE);
-    _utils_depositV2(100e18, WETH);
+    _utils_depositV2_mainnet(100e18, WETH);
     assertApproxEqAbs(compoundV2.getDepositBalanceV2(ALICE, address(cETHV2)), 100e18, 100e18 / 10);
     assertEq(IERC20(WETH).balanceOf(address(compoundV2)), 0);
 
